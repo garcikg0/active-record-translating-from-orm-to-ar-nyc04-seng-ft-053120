aa259f8ccab1571b4e868abdaded3f631b50468e
garcikg0 <kg.gar91@gmail.com>
2020-07-03 11:52:57 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,11 @@
-class Dog
+class Dog < ActiveRecord::Base
+
+    # ActiveRecord::Base.establish_connection(
+    #     :adapter => "sqlite3",
+    #     :database => "db/dogs.db"
+    # )
+
+    # sql = <<-SQL
+    # CREATE TABLE IF 
+
 end
